88d43d81b5dcc7dfec5c92be045dffdc18cac2c3
Spyro <epicdudeness677@gmail.com>
2020-12-11 00:52:15 +0100

    Created Scriptable Object Event System.
    Created a new Scriptable Object Event System.
    - It allows you to create assets inside Unity to use as Events/Listeners
    -Event types go from void to Counting Entities (kill/gather), Time (Wait until x or do until x) as well as Defend (health value is less or equal to 0 or health value is higher than 0).
    -The system is flexible enough to be integrated into the other systems. One example of this is the Interaction system doing a Count Entity event by calling the Event Asset directly.
    
    The plan is to fully replace the messaging system and use this Scriptable Object one as one can create multiple types of Event Assets to use in various contexts. Furthermore, similar logic is used to assign and/or remove listeners from the event asset itself, meaning that the upgrade process should take minimal effort (minus the sheer amount of scripts to update).

---
+++ Assets/Scripts/Interactivity/Components/CountdownEvent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using Interactivity.Events;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Interactivity.Components
+{
+    [CreateAssetMenu(fileName = "New Time Condition", menuName = "Event/Time Condition", order = 0)]
+    public class TimedCondition : CustomEvent
+    {
+        private float _startingTime;
+
+        private void OnEnable()
+        {
+            _startingTime = 0;
+        }
+
+        public override object Raise(object[] args)
+        {
+            int i = 0;
+            if (args[0] is int targetTime)
+                i = targetTime;
+            if (_startingTime == 0) ResetCount();
+            else if (Time.time - _startingTime >= i)
+            {
+                object[] newArgs = new object[args.Length - 1];
+                Array.Copy(args, 1, newArgs, 0, newArgs.Length);
+                return gameEvent?.Invoke(newArgs);
+            }
+
+            return default;
+        }
+
+        private IEnumerator DelayRaise()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void ResetCount()
+        {
+            _startingTime = Time.time;
+        }
+    }
+}
